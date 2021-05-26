77ed43602f96b40eee4245346c41f91c93975110
Sam Limpic <samlimpic@gmail.com>
2021-05-26 16:24:24 -0600

    initialized

---
+++ auth.server/Services/AccountsService.cs
@@ -0,0 +1,25 @@
+using System;
+using auth.server.Models;
+using auth.server.Repositories;
+
+namespace auth.server.Services
+{
+    public class AccountsService
+    {
+        private readonly AccountsRepository _repo;
+
+        public AccountsService(AccountsRepository repo)
+        {
+            _repo = repo;
+        }
+        internal Account GetOrCreateAccount(Account userInfo)
+        {
+            Account account = _repo.GetById(userInfo.Id);
+            if (account == null)
+            {
+                return _repo.Create(userInfo);
+            }
+            return account;
+        }
+    }
+}
